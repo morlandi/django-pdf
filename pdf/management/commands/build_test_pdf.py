40a8dbadff1a52d02e0d573e8b6f65776d7d9b3a
Mario Orlandi <morlandi@brainstorm.it>
2020-11-19 19:32:55 +0100

    [wip] bar and pie plots

--- pdf/management/commands/build_test_pdf.py
+++ pdf/management/commands/build_test_pdf.py
@@ -2,48 +2,116 @@
 from __future__ import print_function
 import os
 import datetime
+import json
+import pprint
+import argparse
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from pdf.utils import build_pdf_document
 from pdf.views import PdfTestView
+from pdf.plot import build_plot_from_data
 
+        # fonts = sorted(set([f.name for f in matplotlib.font_manager.fontManager.ttflist]))
+        # print(fonts)
+
+        # rcParams['font.family'] = 'Tahoma'
 
 class Command(BaseCommand):
-    help = u'Build a test PDF document'
+    help = "Build a test PDF document (or plot)"
+    epilog = """
+Sample usages:
+
+
+python manage.py build_test_pdf test.pdf -o
+
+
+python manage.py build_test_pdf test.png -o -p '{"labels": ["sin", "cos"], "x": [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5], "columns": [[0.0, 9.09, -7.57, -2.79, 9.89, -5.44, -5.37, 9.91, -2.88, -7.51], [20.0, -13.07, -2.91, 16.88, -19.15, 8.16, 8.48, -19.25, 16.68, -2.56]]}' --plot_font Tahoma
+
+
+python manage.py build_test_pdf test.png -o --plot_type horizontalBar -p '{"labels": [null], "columns": [[1085, 839, 340, 285, 270, 211, 210, 177, 161, 141, 139, 137, 137, 134, 124, 122, 119, 116, 112, 108]], "colors": [["#f2f6ee", "#353a3a", "#fcfcfc", "#a23c3a", "#ffd100", "#0f6fa8", "#00968f", "#33b3ad", "#c80000", "#a83e3b", "#244f28", "#b8c0c2", "#586062", "#655580", "#6fd3d1", "#d0cd1d", "#cacac5", "#828b88", "#148396", "#bec8d1"]], "x": ["CW051W", "AC144N", "0011/Sugar Dust-EA Semibrillo Sipa Alfa-Sipamundo-Base Master", "AC118R", "AC105Y", "AC079N", "7116A", "7115D", "1109/Party Time-EA Semibrillo Sipa Alfa-Sipamundo-Base Master", "AC111R", "0535/Zen Retreat-EA Semibrillo SipaAlfa-Sioamundo-BaseMaster", "0546/Paternoster-EA Semibrillo Sipa Alfa-Sipamundo-Base Master", "0534/Subtle Shadow-EA Semibrillo Sipa Alfa-Sipamundo-Base Master", "7006N", "7114M", "7216N", "8792W/Monorail Silver CGl -E.A. P. y F. BioTech-Fandex Millennium-Base Blanca", "0533/Techile-EA Semibrillo Sipa Alfa-Sipamundo-Base Master", "0682/Pleasant Stream-EA Semibrillo Sipa Alfa-Sipamundo-Base Master", "0510/Sacred Spring-EA Semibrillo Sipa Alfa-Sipamundo-Base Master"]}'
+
+
+python manage.py build_test_pdf test.png -o -p '{"labels": ["Total", "Custom", "Stock"], "columns": [[4, 13, 101, 78, 94, 152, 198, 337, 329, 290, 278, 295, 448, 353, 326, 512, 559, 408, 512, 440, 815, 1390, 819, 1562, 2615, 2217, 2352, 1929, 3469, 5791, 6900, 6195], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 14, 36, 67, 8, 46, 67, 519, 585, 559, 433], [4, 13, 101, 78, 94, 152, 198, 337, 329, 290, 278, 295, 448, 353, 326, 512, 559, 408, 512, 415, 815, 1390, 805, 1526, 2548, 2209, 2306, 1862, 2950, 5206, 6341, 5762]], "colors": ["rgba(64, 113, 191, 0.2)", "rgba(191, 64, 64, 0.0)", "rgba(26, 179, 148, 0.0)"], "x": ["03/01/2018", "04/01/2018", "05/01/2018", "06/01/2018", "07/01/2018", "08/01/2018", "09/01/2018", "10/01/2018", "11/01/2018", "12/01/2018", "01/01/2019", "02/01/2019", "03/01/2019", "04/01/2019", "05/01/2019", "06/01/2019", "07/01/2019", "08/01/2019", "09/01/2019", "10/01/2019", "11/01/2019", "12/01/2019", "01/01/2020", "02/01/2020", "03/01/2020", "04/01/2020", "05/01/2020", "06/01/2020", "07/01/2020", "08/01/2020", "09/01/2020", "10/01/2020"]}'
+
+
+python manage.py build_test_pdf test.png -o --plot_type pie -p '{"labels": [], "columns": [[1498, 1217, 528, 479, 363, 353]], "colors": [["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"]], "x": ["255: TOO LOW WATER LEVEL", "210: DOOR OPEN", "211: COVERS NOT AVAILABLE", "2000: Paper end", "1000: Scale not responding", "895: CAN_ABSENT_DURING_DISPENSING"]}'
+
+
+python manage.py build_test_pdf test.png -o --plot_type doughnut -p '{"labels": [], "columns": [[1498, 1217, 528, 479, 363, 353]], "colors": [["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"]], "x": ["255: TOO LOW WATER LEVEL", "210: DOOR OPEN", "211: COVERS NOT AVAILABLE", "2000: Paper end", "1000: Scale not responding", "895: CAN_ABSENT_DURING_DISPENSING"]}'
+
+"""
+
+    def create_parser(self, prog_name, subcommand, **kwargs):
+        if self.epilog:
+            kwargs.update({
+                'epilog': self.epilog,
+            })
+        parser = super().create_parser(prog_name, subcommand, **kwargs)
+        if self.epilog:
+            parser.formatter_class =  argparse.RawTextHelpFormatter
+        return parser
 
     def add_arguments(self, parser):
-        parser.add_argument("--filepath", "-f", help='ouput file path')
+        parser.add_argument("filepath", help='ouput file path (use .pdf or .png extension as required')
+        parser.add_argument('--open', '-o', action='store_true', help="open resulting file")
+        parser.add_argument("--plot_data", "-p", help='JSON plot data to build a bitmap (instead of a pdf)')
+        parser.add_argument('--plot_type', "-t", choices=['line', 'bar', 'horizontalBar', 'pie', 'doughnut', ], default="line")
+        parser.add_argument('--plot_font', )
+        parser.add_argument('--list_fonts', "-l", action='store_true', help="list matplotlib fonts and exit")
+
+    def list_fonts(self):
+        import matplotlib
+        fonts = list(set([f.name for f in matplotlib.font_manager.fontManager.ttflist]))
+        sorted_fonts = sorted(fonts, key=str.casefold)
+        pprint.pprint(sorted_fonts)
+
+    def select_font(self, font):
+        import matplotlib
+        matplotlib.rcParams['font.family'] = font
 
     def handle(self, *args, **kwargs):
 
-        # Create a View to work with
-        view = PdfTestView()
-        context = view.get_context_data()
+        if kwargs['list_fonts']:
+            # list fonts and exit
+            self.list_fonts()
+            return
+
+        if kwargs['plot_font']:
+            self.select_font(kwargs['plot_font'])
+
 
         # Use either user supplied filepath, or the a default name provided by the view
+        expected_file_extension = '.pdf'
         filepath = kwargs['filepath']
-        if not filepath:
-            filepath = view.build_filename(extension="pdf")
+        filename, file_extension = os.path.splitext(filepath)
+
+        # Retrieve and deserialized user-supplied plot data
+        plot_data = kwargs['plot_data']
+        if plot_data is not None:
+            plot_data = json.loads(plot_data)
+            expected_file_extension = '.png'
 
         # File cleanup
+        assert file_extension.lower() == expected_file_extension, "Wrong extension: %s" % file_extension
         if os.path.isfile(filepath):
             os.remove(filepath)
 
         with open(filepath, 'wb') as f:
-            view.render_as_pdf_to_stream('', context, f)
-
-            # build_pdf_document(
-            #     base_url='',
-            #     debug=settings.DEBUG,
-            #     title='test',
-            #     print_date=datetime.datetime.now(),
-            #     extra_context={},
-            #     styles_template_name='pdf/pages/test.css',
-            #     body_template_name='pdf/pages/test.html',
-            #     header_template_name='pdf/header.html',
-            #     footer_template_name='pdf/footer.html',
-            #     output=f,
-            #     #format='pdf',
-            # )
+            if plot_data is not None:
+                image = build_plot_from_data(
+                    plot_data,
+                    chart_type=kwargs['plot_type'],
+                    as_base64=False,
+                    dpi=300,
+                    ylabel='test plot',
+                )
+                f.write(image)
+            else:
+                # Create a View to work with
+                view = PdfTestView()
+                context = view.get_context_data()
+                view.render_as_pdf_to_stream('', context, f)
 
         print('Result in: "%s"' % os.path.abspath(filepath))
+        if kwargs['open']:
+            os.system('open %s' % filepath)
--- pdf/plot.py
+++ pdf/plot.py
@@ -6,7 +6,7 @@ from PIL import Image as PILImage
 import datetime
 
 
-def build_plot_from_data(data, chart_type='line', as_base64=False, plot_colors=None, dpi=300):
+def build_plot_from_data(data, chart_type='line', as_base64=False, dpi=300, ylabel=''):
     """
     Build a plot from given "data";
     Returns: a bitmap of the plot
@@ -17,7 +17,6 @@ def build_plot_from_data(data, chart_type='line', as_base64=False, plot_colors=N
     Keyword arguments:
     data -- see sample_line_plot_data() for an example; if None, uses sample_line_plot_data()
     as_base64 -- if True, returns the base64 encoding of the bitmap
-    plot_colors -- an array of color codes to cycle over; if None, uses default colors
     dpi -- bitmap resolution
 
     Data layout
@@ -36,7 +35,6 @@ def build_plot_from_data(data, chart_type='line', as_base64=False, plot_colors=N
     --------------- ------------------------------------------------------------
     """
 
-
     if data == None:
         data = sample_line_plot_data()
 
@@ -47,8 +45,18 @@ def build_plot_from_data(data, chart_type='line', as_base64=False, plot_colors=N
         try:
             with io.BytesIO() as buffer:
 
+                colors = data['colors'] if 'colors' in data else default_plot_colors()
+                if len(colors)>0 and type(colors[0]) == list:
+                    colors = colors[0]
+
                 if chart_type == 'line':
-                    _build_line_plot_image(data, buffer, plot_colors=plot_colors, dpi=dpi)
+                    _build_line_plot_image(data, colors, buffer, dpi=dpi, ylabel=ylabel)
+                elif chart_type in ['bar', 'horizontalBar', ]:
+                    _build_bar_plot_image(data, colors, buffer, dpi=dpi, ylabel=ylabel, horizontal=(chart_type == 'horizontalBar'))
+                elif chart_type == 'pie':
+                    _build_pie_plot_image(data, colors, buffer, dpi=dpi, ylabel=ylabel)
+                elif chart_type == 'doughnut':
+                    _build_doughnut_plot_image(data, colors, buffer, dpi=dpi, ylabel=ylabel)
                 else:
                     raise Exception('Unknown chart_type "%s"' % chart_type)
 
@@ -68,13 +76,18 @@ def build_plot_from_data(data, chart_type='line', as_base64=False, plot_colors=N
 
 
 def sample_line_plot_data():
-    n = 100
+
+    def real_value(value, decimals=2):
+        text = '%.*f' % (decimals, value)
+        return float(text)
+
+    n = 10
     data = {
         'labels': ["sin", "cos"],
         'x': [0.5 * i for i in range(n)],
         'columns': [
-            [10.0 * math.sin(i/2.0) for i in range(n)],
-            [20.0 * math.cos(i/4.0) for i in range(n)],
+            [real_value(10.0 * math.sin(i*2.0)) for i in range(n)],
+            [real_value(20.0 * math.cos(i*4.0)) for i in range(n)],
         ]
     }
     return data
@@ -92,8 +105,8 @@ def build_random_image():
     return image_content
 
 
-def default_plot_colors():
-    return [
+def default_plot_colors(n = None):
+    default_colors = [
         # Dygraphs defaults
         #"rgb(0,85,128)", "rgb(85,128,0)", "rgb(0,0,128)", "rgb(0,128,0)", "rgb(85,0,128)", "rgb(0,128,85)", "rgb(128,0,85)", "rgb(128,85,0)"
         #'#97742F', '#015480', '#648B18', '#292894', '#007F01', '#4C0E72', '#008055', '#800055',
@@ -101,6 +114,15 @@ def default_plot_colors():
         # CanvasJS defaults
         '#369EAD', '#C24642', '#7F6084', '#86B402', '#52514E', '#C8B631', '#6DBCEB', '#52514E',
     ]
+
+    if n is None:
+        colors = default_colors[:]
+    else:
+        colors = []
+        m = len(default_colors)
+        for i in range(n):
+            colors.append(default_colors[i % m])
+    return colors
 
 def _format_x_time(seconds, x):
     text = str(datetime.timedelta(seconds=seconds))
@@ -120,8 +142,8 @@ def _plot_color(plot_colors, index):
             left_brace_position = rgb_string.find('(')
             right_brace_position = rgb_string.find(')')
             content = rgb_string[left_brace_position + 1:right_brace_position]
-            tokens = [int(t) for t in content.split(',')]
-            color_code = '#%02x%02x%02x' % (tokens[0], tokens[1], tokens[2], )
+            tokens = [t.strip() for t in content.split(',')]
+            color_code = '#%02x%02x%02x' % (int(tokens[0]), int(tokens[1]), int(tokens[2]), )
         except:
             color_code = '#000000'
         return color_code
@@ -136,7 +158,7 @@ def _plot_color(plot_colors, index):
     return color_code
 
 
-def _build_line_plot_image(plot_data, output_buffer, plot_colors, dpi):
+def _build_line_plot_image(plot_data, plot_colors, output_buffer, dpi, ylabel):
     """
     Read rectangular data from given "input_filename" CSV file, and build a bitmap with the plot;
     the result is saved in "output_filename"
@@ -149,27 +171,172 @@ def _build_line_plot_image(plot_data, output_buffer, plot_colors, dpi):
 
     matplotlib.use('agg')
 
-    if plot_colors is None:
-        plot_colors = default_plot_colors()
+    if 'x' in plot_data:
+        x_shared = plot_data['x']
+    else:
+        x_shared = None
 
-    x = plot_data['x']
-    num_cols = len(plot_data['columns'])
-
-    # Scan columns
-    # for index, y in enumerate(data['columns']):
-    #     # add this column to chart
-    #     color = _rgb_string_to_color_code(chart_color(index))
-    #     plt.plot(x, y, color, linewidth=1)
     formatter = matplotlib.ticker.FuncFormatter(_format_x_time)
     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8,3), dpi=dpi, tight_layout=True)
     axes.xaxis.set_major_formatter(formatter)
-    for index, y in enumerate(plot_data['columns']):
-        # add this column to chart
+    for index, values in enumerate(plot_data['columns']):
+
+        if x_shared is not None:
+            x = x_shared
+            y = values
+        else:
+            x = [item['x'] for item in values]
+            y = [item['y'] for item in values]
+
+        #axes.plot(x, y, color, linewidth=1)
+        assert len(x) == len(y)
         color = _plot_color(plot_colors, index)
         axes.plot(x, y, color, linewidth=1)
+        # # add this column to chart
 
-    # plt.ylabel('some numbers')
-    plt.grid()
+    plt.ylabel(ylabel)
+    if plot_data.get('labels', []):
+        plt.legend(plot_data['labels'])
+    plt.grid(axis='y', color='#ccc')
+
+    #plt.grid()
     plt.savefig(output_buffer)  # , dpi=600)
 
 
+def ellipsis(text, max_length):
+    if text is None:
+        return ''
+    text = str(text)
+    if len(text) > max_length:
+        text = text[:max_length - 1] + '…'
+    return text
+
+
+def _build_bar_plot_image(plot_data, plot_colors, output_buffer, dpi, ylabel, horizontal=False):
+    import matplotlib
+    from matplotlib import pyplot as plt
+
+    # Solve "'NSWindow drag regions should only be invalidated on the Main Thread!' - macos/python"
+    # https://github.com/matplotlib/matplotlib/issues/14304#issuecomment-545717061
+    matplotlib.use('agg')
+
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 6), dpi=dpi, tight_layout=True)
+
+    values = plot_data['columns'][0]
+    labels = [ellipsis(l, 20) for l in plot_data['x']]
+    assert len(values) == len(labels)
+
+    colors = [_plot_color(plot_colors, index) for index in range(len(values))]
+    assert len(values) == len(colors)
+
+    # Higher values at top
+    values.reverse()
+    labels.reverse()
+    colors.reverse()
+
+    if horizontal:
+        axes.barh(labels, values, color=colors)
+    else:
+        axes.bar(labels, values, color=colors)
+
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+
+    #
+    # https://mode.com/example-gallery/python_horizontal_bar/
+    #
+
+    # Despine
+    axes.spines['right'].set_visible(False)
+    axes.spines['top'].set_visible(False)
+    axes.spines['left'].set_visible(False)
+    axes.spines['bottom'].set_visible(False)
+
+    # Draw vertical axis lines
+    vals = axes.get_xticks()
+    for tick in vals:
+        axes.axvline(x=tick, linestyle='solid', alpha=0.4, color='#eeeeee', zorder=1)
+
+    # # Switch off ticks
+    # axes.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
+
+    # # Format y-axis label
+    # axes.xaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter('{x:,g}'))
+
+
+    #plt.legend(plot_data['labels'], fontsize=10, )
+    #plt.ylabel(ylabel)
+    #plt.grid(axis='y', color='#ccc')
+    plt.savefig(output_buffer)  # , dpi=600)
+
+
+def _build_pie_plot_image(plot_data, plot_colors, output_buffer, dpi, ylabel):
+
+    def func(pct, allvals):
+        #absolute = int(pct/100.*np.sum(allvals))
+        absolute = int(pct/100. * sum(allvals))
+        #return "{:.1f}%\n({:d} g)".format(pct, absolute)
+        return "{:d}\n({:.1f}%)".format(absolute, pct)
+
+    import matplotlib
+    from matplotlib import pyplot as plt
+
+    # Solve "'NSWindow drag regions should only be invalidated on the Main Thread!' - macos/python"
+    # https://github.com/matplotlib/matplotlib/issues/14304#issuecomment-545717061
+    matplotlib.use('agg')
+
+    # https://matplotlib.org/3.1.1/gallery/pie_and_polar_charts/pie_and_donut_labels.html
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 6), dpi=dpi, tight_layout=True, subplot_kw=dict(aspect="equal"))
+
+    values = plot_data['columns'][0]
+    labels = plot_data['x']
+    colors = [_plot_color(plot_colors, index) for index in range(len(values))]
+    assert len(values) == len(labels)
+    assert len(values) == len(colors)
+
+    # values.reverse()
+    # labels.reverse()
+    # colors.reverse()
+
+    wedges, texts, autotexts = axes.pie(values, autopct=lambda pct: func(pct, values), textprops=dict(color="w"), colors=colors, startangle=90)
+    axes.legend(wedges, labels, title="", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+    plt.setp(autotexts, size=8, weight="bold")
+
+    if ylabel:
+        axes.set_title(ylabel)
+
+    plt.savefig(output_buffer)  # , dpi=600)
+
+
+def _build_doughnut_plot_image (plot_data, plot_colors, output_buffer, dpi, ylabel):
+    import matplotlib
+    from matplotlib import pyplot as plt
+    import numpy as np
+
+    # Solve "'NSWindow drag regions should only be invalidated on the Main Thread!' - macos/python"
+    # https://github.com/matplotlib/matplotlib/issues/14304#issuecomment-545717061
+    matplotlib.use('agg')
+
+    # https://matplotlib.org/3.1.1/gallery/pie_and_polar_charts/pie_and_donut_labels.html
+
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 6), dpi=dpi, tight_layout=True, subplot_kw=dict(aspect="equal"))
+
+    recipe = ["225 g flour", "90 g sugar", "1 egg", "60 g butter", "100 ml milk", "1/2 package of yeast"]
+    data = [225, 90, 50, 60, 100, 5]
+    wedges, texts = axes.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        axes.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y), horizontalalignment=horizontalalignment, **kw)
+
+    if ylabel:
+        axes.set_title(ylabel)
+    plt.savefig(output_buffer)  # , dpi=600)
